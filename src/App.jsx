fe630ab19fc47323b15d78b86fe595eca54d4742
Rayson Kaelbling <raysonkaelbling@outlook.com>
2021-10-02 01:11:56 -0500

    Fix scrolling and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Main from './Main';
 import Canvas from './Canvas';
@@ -8,12 +8,12 @@ import Canvas from './Canvas';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter baseName="/">
         <Switch>
           <Route path="/" exact component={() => <Main />} />
           <Route path="/canvas" exact component={() => <Canvas />} />
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
