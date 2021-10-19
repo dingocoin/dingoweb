b56ac47e75b776d2b7fff38b265b39007923865b
Rayson Kaelbling <raysonkaelbling@outlook.com>
2021-10-19 17:47:25 -0500

    please work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.scss';
 import React from 'react';
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Main from './Main';
+import Canvas from './Canvas';
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('UA-210617812-1');
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
     <div className="App">
-      <Main />
+      <HashRouter baseName="/">
+        <Switch>
+          <Route path="/home" exact component={() => <Main />} />
+          <Route path="/canvas" exact component={() => <Canvas />} />
+          <Redirect exact from="/" to="/home" />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
