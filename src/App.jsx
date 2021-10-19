93353a4ecf49458f9985e587595965d5e99fece6
Rayson Kaelbling <raysonkaelbling@outlook.com>
2021-10-19 17:05:23 -0500

    Simplify and add stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.scss';
 import React from 'react';
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Main from './Main';
-import Canvas from './Canvas';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter baseName="/">
-        <Switch>
-          <Route path="/home" exact component={() => <Main />} />
-          <Route path="/canvas" exact component={() => <Canvas />} />
-          <Redirect exact from="/" to="/home" />
-        </Switch>
-      </HashRouter>
+      <Main />
     </div>
   );
 }
