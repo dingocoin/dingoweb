304a402d953b759010aa91e24bde35b4d6a23120
Rayson Kaelbling <raysonkaelbling@outlook.com>
2021-11-19 12:35:38 -0600

    Update amount

--- src/Main.jsx
+++ src/Main.jsx
@@ -250,7 +250,7 @@ function Main() {
                 <span>Currently raising funds for a Hotbit listing.</span>
                 <img alt="" src={Parrot1Logo}/>
               </h5>
-              <ProgressBar animated now={39865 * 100 / 50000} className="fund-raising-progress" label="$39,865 / $50,000" />
+              <ProgressBar animated now={42865 * 100 / 50000} className="fund-raising-progress" label="$42,865 / $50,000" />
               <h5 className="fundraising">
                 <img alt="" src={Parrot2Logo}/>
                 <span>Join our Discord to contribute!</span>
