ab351566ed235b5541b1f298483cfe553dfb5d36
Rayson Kaelbling <raysonkaelbling@outlook.com>
2021-12-07 21:29:48 -0600

    Fix format

--- src/Main.jsx
+++ src/Main.jsx
@@ -784,7 +784,7 @@ function Main() {
                   {burnBoardList.map((x) => (
                     <tr key={x.rank} className={x.rank === 1 ? "gold" : x.rank === 2 ? "silver" : x.rank === 3 ? "bronze" : ""}>
                       <td className="col-1">{x.rank}</td>
-                      <td className="col-8">{x.amount}</td>
+                      <td className="col-8">{parseFloat(x.amount).toLocaleString()}</td>
                       <td className="col-3">{x.data === null ? "" : Buffer.from(x.data, 'hex').toString('ascii')}</td>
                     </tr>
                   ))}
