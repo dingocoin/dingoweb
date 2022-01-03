62e09429c25625461010681a722c606715a16f0f
Rayson Kaelbling <raysonkaelbling@outlook.com>
2022-01-03 08:54:24 -0600

    Cleanup. Sitemap test.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import React from 'react';
 import Main from './Main';
 import ReactGA from 'react-ga';
 
+// Routing.
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   ReactGA.initialize('UA-210617812-1');
   ReactGA.pageview(window.location.pathname + window.location.search)
 
   return (
     <div className="App">
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="*" element={<Main/>}>
+          </Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
